ada4ee7ca10fdca811fa0c08c410852852034335
acw <aldcwatson@gmail.com>
2023-10-26 20:54:09 -0700

    Added configuration file reading and app configuring. Limit to just one raw input file

--- checkin_manager.py
+++ checkin_manager.py
@@ -10,72 +10,81 @@
 import argparse
 import csv
 from checkins import add_checkin, get_last_checkin_repr
+import config
 from storage import start_database, close_database,\
                     create_checkin_table, StorageException
 
-CHECKIN_NUM_COLUMNS = 7 # TODO - set this value via a configuration file
-CHECKIN_FIELD_NAMES = ('week_number', 
-                       'callsign',
-                       'transport_mode',
-                       'rms_gateway',
-                       'rms_frequency',
-                       'location',
-                       'state')
 # ------------------------------------------------------------------------------
 def process_command_line():
     parser = argparse.ArgumentParser(description='Clean a check-in CSV file.')
     parser.add_argument('-n',
                         '--nheaders',
                         type=int,
-                        default=0,
+                        default=None,
                         help='The number of header lines to ignore. Default 0')
 
     # The file list is whatever we get as optional arguments at the end of the
     # command line. There must be one file name, however and nargs catches this
     # with the '+' symbol (at least one).
-    parser.add_argument('file_list',
+    parser.add_argument('-f',
+                        '--raw_checkins_filename',
                         metavar='File',
                         type=str,
-                        nargs='+', # Plus means at least one file name arg.
-                        help='A list of file names to process')
+                        default=None,
+                        help='The name of the raw checkins file to process')
     args = parser.parse_args()
-    return (args.nheaders, args.file_list)
+    return (args.nheaders, args.raw_checkins_filename)
 # -----------------------------------------------------------------------------
 
-def scan_file(csv_file_name, num_header_lines):
+def scan_file(csv_file_name: str, num_header_lines: int, col_names: list[str]) -> None:
+    num_cols = len(col_names)
     with open(csv_file_name, "r", newline='') as csvfile:
-        reader = csv.DictReader(csvfile, fieldnames=CHECKIN_FIELD_NAMES)
+        reader = csv.DictReader(csvfile, fieldnames=col_names)
         header_line_count = 0
         for checkin_line in reader:
             if header_line_count < num_header_lines:
                 header_line_count += 1
                 continue
 
-            if len(checkin_line.keys()) != CHECKIN_NUM_COLUMNS:
+            if len(checkin_line.keys()) != num_cols:
                 raise ValueError(f'Check-in has invalid num cols: {checkin_line}')
             clean_checkin_line = dict((k.strip(), v.strip()) for k, v in checkin_line.items())
-            add_checkin(int(clean_checkin_line['week_number']),
-                        clean_checkin_line['callsign'],
-                        clean_checkin_line['transport_mode'],
-                        clean_checkin_line['rms_gateway'],
-                        float(clean_checkin_line['rms_frequency']),
-                        clean_checkin_line['location'],
-                        clean_checkin_line['state'])
+            add_checkin(int(clean_checkin_line[col_names[0]]),
+                        clean_checkin_line[col_names[1]],
+                        clean_checkin_line[col_names[2]],
+                        clean_checkin_line[col_names[3]],
+                        float(clean_checkin_line[col_names[4]]),
+                        clean_checkin_line[col_names[5]],
+                        clean_checkin_line[col_names[6]])
 
             print(f'{get_last_checkin_repr()}')
 
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 def main():
-    # Set up command line parsing
-    (num_header_lines, csv_file_list) = process_command_line()
+    # Set up command line parsing. Command line takes precedence over the
+    # config file to allow quick experiments without having to change the
+    # config.
+    config.load_config_file('config.json')
+    raw_checkins_filename = config.get_raw_checkin_filename()
+    raw_checkins_col_names = config.get_raw_checkin_fieldnames()
+    num_header_lines = config.get_num_header_lines()
+    database_name = config.get_database_name()
+    
+    (cl_num_header_lines, cl_raw_checkins_filename) = process_command_line()
 
-    start_database('basic_test.db')
+#   TODO: Need to check to see if config file values actually exist
+    if cl_num_header_lines is not None:
+        num_header_lines = cl_num_header_lines
+
+    if cl_raw_checkins_filename is not None:
+        raw_checkins_filename = cl_raw_checkins_filename
+
+    start_database(database_name)
     create_checkin_table()
 
-    for csv_file_name in csv_file_list:
-        scan_file(csv_file_name, num_header_lines)
+    scan_file(raw_checkins_filename, num_header_lines, raw_checkins_col_names)
     close_database()
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 if __name__ == '__main__':
     main()
--- config.py
+++ config.py
@@ -7,23 +7,38 @@ class ConfigException(Exception):
     def __init__(self, msg):
         super().__init__(msg)
 #------------------------------------------------------------------------------
-def load_config_file(filename):
+def load_config_file(filename) -> dict|None:
     if filename is None:
         raise ConfigException('No configuration file')
-    
+
     global _current_config
+    config = None
     if _current_config is None:
         # Only load the config once
-        config = None
         with open(filename, "r") as fp:
             config = json.load(fp)
 
         # TODO: might want to do some config value checking here
         _current_config = config
-    return _current_config
+        return config
 #------------------------------------------------------------------------------
-def get_config() -> dict | None:
-    return _current_config
+def get_database_name() -> str | None:
+    return _current_config['database_name']
+
+#------------------------------------------------------------------------------
+def get_raw_checkin_filename() -> str | None:
+    return _current_config['raw_checkin_filename']
+
+#------------------------------------------------------------------------------
+def get_raw_checkin_fieldnames() -> list[str] | None:
+    return _current_config['raw_checkin_field_names']
+
+#------------------------------------------------------------------------------
+def get_num_header_lines() -> int | None:
+    return int(_current_config['num_header_lines'])
+#------------------------------------------------------------------------------
+
+
 #------------------------------------------------------------------------------
 # Test code below here
 #------------------------------------------------------------------------------
@@ -37,4 +52,8 @@ if __name__ == '__main__':
 #------------------------------------------------------------------------------
 # External interface
 #------------------------------------------------------------------------------
-__all__ = ['load_config_file', 'get_config', 'ConfigException']
+__all__ = ['load_config_file',
+           'get_num_header_lines',
+           'get_raw_checkin_filename',
+           'get_raw_checkin_fieldnames',
+           'ConfigException']
