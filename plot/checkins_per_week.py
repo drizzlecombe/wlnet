38741504bee1a3d63600c1783ae0a5730eb26238
acw <aldcwatson@gmail.com>
2023-10-30 21:32:27 -0700

    Cleaned up the code and made some simplifications

--- plot/checkins_per_week.py
+++ plot/checkins_per_week.py
@@ -5,10 +5,11 @@
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 import db_access as db
 
-def _get_weekly_participation(database_name: str) -> list[tuple[int, int]]:
+def _get_total_checkins_per_week(database_name: str) -> list[tuple[int, int]]:
     db.start_database(database_name)
 
     q = """select week_number, count(*) 
@@ -40,9 +41,9 @@ def exponential_smooth(data: list[int], smoothing_factor: float=0.30) -> list[fl
     return smoothed
 
 #------------------------------------------------------------------------------
-def draw_chart(x: list[int], y: list[int], smoothed: list[float]) -> None:
+def draw_checkins_per_week_chart(x: list[int], y: list[int], smoothed: list[float]) -> None:
     # Just one subplot in the figure. 
-    fig, ax = plt.subplots(1, 1, figsize=(10, 3))
+    fig, ax = plt.subplots(1, 1, figsize=(10, 4))
     # Plot the actual checkin values
     ax.plot(x, y, 'o:', ms='5', mec='1.0', linewidth='1.0', color='darkgreen', label='# checkins')
     # Plot the (hardwired) mean. TODO: Turn this into a function parameter
@@ -54,30 +55,81 @@ def draw_chart(x: list[int], y: list[int], smoothed: list[float]) -> None:
     # Turn on the legend. Default placement is fine.
     ax.legend()
     plt.show()
+
 #------------------------------------------------------------------------------
+def draw_checkins_per_week_bar(num_checkins_dist: list[tuple[int, int]]) \
+                                 -> None:
+    """Draws a bar chart showing the sum of number of particpants/week
+    
+    num_chkins_dist - a list containing tuples. The tuples consist of:
+       - count of checkins for a week
+       - number of times that number of checkins occurred
+    """
+    # Just one subplot in the figure.
+    fig, ax = plt.subplots(1, 1, figsize=(6, 4))
+    
+
+    # Hopefully this is enough colours for the number of bars in the chart. If
+    # not , the colours will wrap.
+    # TODO: Find a way of interpolating between darkred and darkgreen using
+    # HTML colour codes.
+    bar_colours = ['darkred', 'firebrick', 'red', 'orangered', 'darkorange',
+               'orange', 'gold', 'yellow', 'greenyellow', 'limegreen',
+               'green', 'darkgreen']
+    
+    # Has unzip() been deprecated?
+    checkins_in_a_week = [k for (k, v) in num_checkins_dist]
+    num_times_checkin_cnt_seen = [v for (k, v) in num_checkins_dist] 
+    ax.bar(checkins_in_a_week, num_times_checkin_cnt_seen, 
+           linewidth=0.5, edgecolor='black',
+           color=bar_colours)
+    # ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    # Plot the actual checkin values
+   
+    ax.set_title('Weekly net participation (distinct operators/week)')
+    ax.set_ylabel('Number of weeks')
+    ax.set_xlabel('Number of operators checking in for any week')
+    ax.yaxis.grid(True, linestyle='dotted')
+    ax.set(xlim=(5, 18), xticks=range(5, 18),
+       ylim=(0, 10), yticks=range(0, 10))
+    plt.show()
+
+#------------------------------------------------------------------------------#------------------------------------------------------------------------------
 def main(database_name: str) -> None:
-    xy_data = _get_weekly_participation(database_name)
+    checkins_by_week_num = _get_total_checkins_per_week(database_name)
 
     num_weeks = 0
     participant_total = 0
     week_numbers: list[int] = []
     week_checkin_counts: list[int] = []
-    for (x, y) in xy_data:
-        print(f'{x}, {y}')
+    aggregated_checkin_counts = {}
+    for (week_no, num_checkins) in checkins_by_week_num:
+        print(f'{week_no}, {num_checkins}')
         num_weeks += 1
-        participant_total += y
-        week_numbers.append(x)
-        week_checkin_counts.append(y)
+        participant_total += num_checkins
+        week_numbers.append(week_no)
+        week_checkin_counts.append(num_checkins)
+
+        # Count the weeks that have exactly n checkins.
+        week_count = aggregated_checkin_counts.setdefault(num_checkins, 0)
+        week_count += 1
+        aggregated_checkin_counts[num_checkins] = week_count
 
     smoothed_week_counts: list[float] = exponential_smooth(week_checkin_counts)    
     mean_participation = float(participant_total) / float(num_weeks)
-
+    
+    # Sort the number of checkins per week seen. Answers the question: 'How
+    # weeks seen n checkins?' 
+    sorted_times_seen_counts = [(k, aggregated_checkin_counts[k]) \
+                                for k in \
+                                    sorted(aggregated_checkin_counts.keys())]
 
     print(f'Mean participation = {mean_participation: 0.2f}')
     print(f'Median participation = {median(week_checkin_counts): 0.2f}')
-
-    draw_chart(week_numbers, week_checkin_counts, smoothed_week_counts)
-
+    print(f'Aggregate checkins: {sorted_times_seen_counts}')
+    draw_checkins_per_week_chart(week_numbers, week_checkin_counts,
+                                 smoothed_week_counts)
+    draw_checkins_per_week_bar(sorted_times_seen_counts)
 #------------------------------------------------------------------------------
 # Programme entry point
 #------------------------------------------------------------------------------
