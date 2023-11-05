958d7ee58d30ec034b6a72fe9f24cb78b2591753
acw <aldcwatson@gmail.com>
2023-11-04 23:26:42 -0700

    Added a second table - one that maps checkin callsigns to current callsigns.

--- db/sqlite_code_examples.sql
+++ db/sqlite_code_examples.sql
@@ -7,6 +7,38 @@ CREATE TABLE if not exists checkins(week_number INTEGER,
                                        location TEXT,
                                           state TEXT);
 
+-- Maps callsigns used to checkin to new callsigns received after the checkin
+-- was made. Most callsigns will map directly onto themselves.
+CREATE TABLE if not exists callsign_map(checkin_callsign TEXT, 
+                                        current_callsign TEXT);
+
+INSERT INTO callsign_map (checkin_callsign, current_callsign)
+VALUES
+('AD7EF', 'AD7EF'),
+('K0AXS', 'K0AXS'),
+('KD7DNM', 'KD7DNM'),
+('KD7PFH', 'KD7PFH'),
+('KF7KXX', 'KF7KXX'),
+('KI7LAG', 'KI7LAG'),
+('KJ7JBE', 'KJ7JBE'),
+('KJ7JCR', 'KJ7JCR'),
+('KJ7YYW', 'W6RKT'),
+('N1ACW', 'N1ACW'),
+('WA6DOZ', 'WA6DOZ'),
+('WA6GBW', 'WA6GBW'),
+('WA6ZLV', 'WA6ZLV'),
+('WH6EWE', 'WH6EWE'),
+('KF0RST', 'KF0RST'),
+('KI7DGC', 'KI7DGC'),
+('N7LIM', 'N7LIM'),
+('WA1TRH', 'WA1TRH'),
+('W6AGZ', 'W6AGZ'),
+('KK7JZB', 'W3STM'),
+('KK7MLS', 'KK7MLS'),
+('AI7MI', 'AI7MI'),
+('W7MML', 'W7MML'),
+('W3STM', 'W3STM'),
+('W6RKT', 'W6RKT');
 
 -- .mode changes the way the output is formatted. There are lots of options!
  .mode markdown
@@ -55,9 +87,12 @@ select week_number, count(*)
 -- callsign checks in at least once using a specific transport mode. This is
 -- true even if the operator checks in more than once using a specific
 -- transport mode, like VARA FM, during a specific week. 
-select callsign, transport_mode, count(*)
-  from (select distinct callsign, transport_mode, week_number
-          from checkins) 
- group by callsign, transport_mode
- order by callsign, transport_mode;
+select cs, mode, count(*)
+  from (select distinct cm.current_callsign as cs, 
+                        transport_mode as mode, 
+                        week_number
+          from checkins, callsign_map as cm
+         where checkins.callsign = cm.checkin_callsign) 
+ group by cs, mode
+ order by cs, mode;
 
